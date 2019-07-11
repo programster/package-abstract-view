ec39fea53ba6d8d6dea5ee7b3207ba6150a4d560
Stuart <stuart.page@diverseinteractive.com>
2019-07-11 15:45:01 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+Abstract View
+========
+
+This is a simple object to make it easy to build object-oriented views.
---
+++ src/AbstractView.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Programster\AbstractView;
+
+
+abstract class AbstractView
+{
+    public function render()
+    {
+        ob_start();
+        $this->renderContent();
+        return ob_get_clean();
+    }
+    
+    public function __toString() { return $this->render(); }
+    
+    abstract protected function renderContent();
+}
